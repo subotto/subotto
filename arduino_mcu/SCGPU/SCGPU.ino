c1f5f87ff8db2ac0c33f1233456e2ae7b1f65612
Enrico Polesel <saggiopol@gmail.com>
2013-02-01 15:57:38 +0100

    Aggiunto supporto per pulsanti

--- arduino_mcu/SCGPU/SCGPU.ino
+++ arduino_mcu/SCGPU/SCGPU.ino
@@ -1,27 +1,52 @@
 //#include <SimpleTimer.h>
 
-#define BLUE1 2
-#define BLUE2 5
-#define RED1 10
-#define RED2 12
+#define BLUE1_PIN 2
+#define BLUE2_PIN 3
+#define RED1_PIN 4
+#define RED2_PIN 5
+
+#define BLUE1_GOAL 1
+#define BLUE2_GOAL 2
+#define RED1_GOAL 3
+#define RED2_GOAL 4
+
+#define BLUE_ADD_PIN 8
+#define BLUE_UNDO_PIN 9
+#define RED_ADD_PIN 10
+#define RED_UNDO_PIN 11
+
+#define BLUE_ADD_PUSH 5
+#define BLUE_UNDO_PUSH 6
+#define RED_ADD_PUSH 7
+#define RED_UNDO_PUSH 8
+
 
 #define TEST_MODE 0
 #define SLAVE_MODE 1
 
 #define GOAL_DELAY 3000
+#define PUSH_DELAY 5000
 
 int mode;
 unsigned long last_goal;
+unsigned long last_push;
 //int sensors_enabled;
 
 //SimpleTimer timer;
 
 void setup()
 {
-  pinMode(BLUE1,INPUT_PULLUP);
-  pinMode(BLUE2,INPUT_PULLUP);
-  pinMode(RED1,INPUT_PULLUP);
-  pinMode(RED2,INPUT_PULLUP);
+  pinMode(BLUE1_PIN,INPUT_PULLUP);
+  pinMode(BLUE2_PIN,INPUT_PULLUP);
+  pinMode(RED1_PIN,INPUT_PULLUP);
+  pinMode(RED2_PIN,INPUT_PULLUP);
+  
+  pinMode(BLUE_ADD_PIN,INPUT_PULLUP);
+  pinMode(BLUE_UNDO_PIN,INPUT_PULLUP);
+  pinMode(RED_ADD_PIN,INPUT_PULLUP);
+  pinMode(RED_UNDO_PIN,INPUT_PULLUP);
+  
+  
   
   pinMode(13,OUTPUT);
   digitalWrite(13,LOW);
@@ -29,7 +54,10 @@ void setup()
   Serial.begin(9600);
   
   mode = TEST_MODE;
-  last_goal = millis();
+//  last_goal = millis();
+//  last_push = millis();
+  last_goal = 0;
+  last_push = 0;
 //  sensors_enabled = 1;
   
 }
@@ -67,13 +95,13 @@ void test_main(int input)
 {
   int result = -1;
   if (input == 1)
-    result = digitalRead(BLUE1);
+    result = digitalRead(BLUE1_PIN);
   else if (input == 2)
-    result = digitalRead(BLUE2);
+    result = digitalRead(BLUE2_PIN);
   else if (input == 3)
-    result = digitalRead(RED1);
+    result = digitalRead(RED1_PIN);
   else if (input == 4)
-    result = digitalRead(RED2);
+    result = digitalRead(RED2_PIN);
   
   if (result != -1)
     Serial.println(result);
@@ -89,26 +117,26 @@ void slave_main(int input)
   if (millis() > last_goal + GOAL_DELAY)
 //  if (sensors_enabled)
   {
-     if (digitalRead(BLUE1))
+     if (digitalRead(BLUE1_PIN))
      {
-       Serial.println(1);
+       Serial.println(BLUE1_GOAL);
        last_goal = millis();
 //       while(digitalRead(BLUE1));
 //        sensors_enabled = 0;
 //        timer.setTimeout(GOAL_DELAY,enable_sensors);
      }
-     if (digitalRead(BLUE2))
+     if (digitalRead(BLUE2_PIN))
      {
-       Serial.println(2);
+       Serial.println(BLUE2_GOAL);
        last_goal = millis();
 //       while(digitalRead(BLUE2));
 //        sensors_enabled = 0;
 //        timer.setTimeout(GOAL_DELAY,enable_sensors);
 
      }
-     if (digitalRead(RED1))
+     if (digitalRead(RED1_PIN))
      {
-       Serial.println(3);
+       Serial.println(RED1_GOAL);
        last_goal = millis();
 //       while(digitalRead(RED1));
 //        sensors_enabled = 0;
@@ -116,9 +144,9 @@ void slave_main(int input)
 
 
      }
-     if (digitalRead(RED2))
+     if (digitalRead(RED2_PIN))
      {
-       Serial.println(4);
+       Serial.println(RED2_GOAL);
        last_goal = millis();
 //       while(digitalRead(RED2));
 //        sensors_enabled = 0;
@@ -128,6 +156,30 @@ void slave_main(int input)
   }
 //  else
 //    Serial.println("not_enable");
+
+  if (millis() > last_push + PUSH_DELAY)
+  {
+    if (digitalRead(BLUE_ADD_PIN))
+    {
+       Serial.println(BLUE_ADD_PUSH);
+       last_push = millis();
+    }
+    if (digitalRead(BLUE_UNDO_PIN))
+    {
+       Serial.println(BLUE_UNDO_PUSH);
+       last_push = millis();
+    }
+    if (digitalRead(RED_ADD_PIN))
+    {
+       Serial.println(RED_ADD_PUSH);
+       last_push = millis();
+    }
+    if (digitalRead(RED_UNDO_PIN))
+    {
+       Serial.println(RED_UNDO_PUSH);
+       last_push = millis();
+    }
+  }
   
 }
 
