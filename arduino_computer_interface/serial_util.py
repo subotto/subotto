47d43aa1b860c84e591d2b1a7d7fda7e31f7bd1d
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2013-02-02 11:17:10 +0100

    Some functionalities for serial_util, but in a bad framework.

--- arduino_computer_interface/serial_util.py
+++ arduino_computer_interface/serial_util.py
@@ -5,6 +5,8 @@ import sys
 import serial
 import time
 
+from opcodes import *
+
 class SubottoSerial:
 
     def __init__(self, port, speed):
@@ -13,9 +15,11 @@ class SubottoSerial:
         self.serial = serial.Serial(port=port, baudrate=speed, timeout=None)
         #self.serial.nonblocking()
 
+    # BASIC IO INTERFACE
+
     def send_number(self, num):
         print >> sys.stderr, "> TO SERIAL PORT: %d" % (num)
-        self.serial.write("%d\n" % (num))
+        self.serial.write("%d" % (num))
         self.serial.flush()
 
     def recv_number(self):
@@ -25,6 +29,29 @@ class SubottoSerial:
 
     def has_data(self):
         return self.serial.inWaiting() > 0
+
+    def send_recv(self, send):
+        self.send_number(send)
+        return self.recv_number()
+
+    def send_expect(self, send, expect):
+        res = self.send_recv(send)
+        return res == expect
+
+    # UPPER LEVEL COMMANDS
+
+    def wait_for_ready(self):
+        num = self.recv_number()
+        return num == SUB_READY
+
+    def request_echo(self):
+        return self.send_expect(COM_ECHO_TEST, SUB_ECHO_REPLY)
+
+    def set_slave_mode(self):
+        return self.send_expect(COM_SET_SLAVE_MODE, SUB_SLAVE_MODE)
+
+    def set_test_mode(self):
+        return self.send_expect(COM_SET_TEST_MODE, SUB_TEST_MODE)
 
 
 if __name__ == '__main__':
