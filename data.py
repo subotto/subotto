0c667851986870f33b087b7aa6dcef029b9279aa
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2013-05-13 18:22:15 +0200

    Database location updated.

--- data.py
+++ data.py
@@ -15,7 +15,7 @@ try:
         database_url = fdata.readline().strip()
 except IOError:
     with open('passwd') as fpasswd:
-        database_url = 'postgresql://subotto:%s@roma.uz.sns.it/subotto_new' % (fpasswd.read().strip())
+        database_url = 'postgresql://subotto:%s@soyuz.uz.sns.it/subotto' % (fpasswd.read().strip())
 db = create_engine(database_url, echo=False)
 #db = create_engine('sqlite:///subotto.sqlite', echo=True)
 Session = sessionmaker(db)
